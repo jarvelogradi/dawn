31966e01251eef8c91d620485db3500902732403
Moises Arvelo <moisesarvelo45@gmail.com>
2022-05-16 03:45:12 -0400

    Fixed Image changeDinamically - Add section custom cart

--- assets/checksDinamically.js
+++ assets/checksDinamically.js
@@ -1,25 +1,28 @@
+const rooturl = "http://127.0.0.1:9292/"
+
 function handleChange(src) {
     let data = JSON.parse(src.value);
     let labelPrice = document.getElementById("price" + data.featured_image.product_id);
+    let productImage = document.getElementById("productImage" + data.featured_image.product_id);
     let inputData = document.querySelector("#inputData" + data.featured_image.product_id);
 
     inputData.value = JSON.stringify(data);
-
     labelPrice.innerHTML = moneyWithCurrency(data.price);
+    productImage.src = data.featured_media.preview_image.src;
     console.log(data);
 }
 
-function moneyWithCurrency(value){
+function moneyWithCurrency(value) {
     let int = parseInt(value);
 
-    let refactorValue = parseFloat(int/100).toFixed(2);
+    let refactorValue = parseFloat(int / 100).toFixed(2);
 
     let finalValue = `$${refactorValue} USD`;
 
     return finalValue;
 }
 
-function handleOnclick(productId, rooturl = "http://127.0.0.1:9292/") {
+function handleOnclick(productId) {
 
     let data = JSON.parse(document.querySelector("#inputData" + productId).value);
 
@@ -39,10 +42,23 @@ function handleOnclick(productId, rooturl = "http://127.0.0.1:9292/") {
         body: JSON.stringify(formData),
     })
         .then((response) => {
-            return response.json();
+            renderSection(".shopify-section-cart-icon-bubble")
         })
         .catch((error) => {
             console.error("Error", error);
         });
 
+}
+
+function renderSection(elementId) {
+    fetch(rooturl + '?section_id=cart-icon-bubble')
+        .then(response => response.text())
+        .then(data => {
+            console.log(data);
+            let innerHTML = new DOMParser()
+                .parseFromString(data, 'text/html')
+                .getElementById('shopify-section-cart-icon-bubble').innerHTML;
+
+            document.getElementById('cart-icon-bubble').innerHTML = innerHTML;
+        });
 }
