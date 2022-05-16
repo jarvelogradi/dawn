99f7aa972f911c1b423a53f8bf0c73eb9670b8d2
Moises Arvelo <moisesarvelo45@gmail.com>
2022-05-16 01:31:59 -0400

    Cambios Gradiweb Input Settings

--- assets/checksDinamically.js
+++ assets/checksDinamically.js
@@ -1,4 +1,48 @@
-function handleChange(src){
-    console.log(src.value);
-     
- }
+function handleChange(src) {
+    let data = JSON.parse(src.value);
+    let labelPrice = document.getElementById("price" + data.featured_image.product_id);
+    let inputData = document.querySelector("#inputData" + data.featured_image.product_id);
+
+    inputData.value = JSON.stringify(data);
+
+    labelPrice.innerHTML = moneyWithCurrency(data.price);
+    console.log(data);
+}
+
+function moneyWithCurrency(value){
+    let int = parseInt(value);
+
+    let refactorValue = parseFloat(int/100).toFixed(2);
+
+    let finalValue = `$${refactorValue} USD`;
+
+    return finalValue;
+}
+
+function handleOnclick(productId, rooturl = "http://127.0.0.1:9292/") {
+
+    let data = JSON.parse(document.querySelector("#inputData" + productId).value);
+
+    let formData = {
+        items: [
+            {
+                id: data.id,
+                quantity: 1,
+            },
+        ],
+    };
+    fetch(rooturl + "cart/add.js", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .catch((error) => {
+            console.error("Error", error);
+        });
+
+}
